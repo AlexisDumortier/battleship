f8e123daa645006744da205b6902311ddb0955e9
Maria Ronauli <mronauli@gmail.com>
2019-12-05 11:55:42 -0700

    Add placing ship tests #test_board_responds_to_place_ship #test_that_cell_receives_ship_after_ship_is_placed #test_that_cells_with_same_ship_have_the_same_ship

--- test/board_test.rb
+++ test/board_test.rb
@@ -50,7 +50,6 @@ class BoardTest < Minitest::Test
   def test_coordinates_are_in_ascending_order
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
     assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
   end
 
@@ -67,9 +66,37 @@ class BoardTest < Minitest::Test
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
-
     assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
     assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+  end
+
+  def test_board_responds_to_place_ship
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    assert_respond_to board, :place_ship
+  end
+
+  def test_that_cell_receives_ship_after_ship_is_placed
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+    assert equal cruiser, cell_1.ship
+    assert_equal cruiser, cell_2.ship
+    assert_equal crusier, cell_3.ship
+  end
+
+  def test_that_cells_with_same_ship_have_the_same_ship
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    assert_equal true, cell_3.ship == cell_2.ship
+    assert_equal true, cell_2.ship == cell_1.ship
+    assert_equal true, cell_1.ship == cell_3.ship
+    assert_equal false, cell_4.ship == cell_2.ship
   end
 
 end
